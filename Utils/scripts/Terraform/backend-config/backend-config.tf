29ce8b0724b489ecaff9d295428e00dfbd987004
Srivatsa Hegde <srhegde@microsoft.com>
2023-07-17 16:44:12 -0700

    add output variables

--- Utils/scripts/Terraform/backend-config/backend-config.tf
+++ Utils/scripts/Terraform/backend-config/backend-config.tf
@@ -40,3 +40,16 @@ resource "azurerm_storage_container" "tfstate" {
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
 }
+
+output "rg_name" {
+  value                = azurerm_resource_group.tfstate.name
+}
+
+output "storage_name" {
+  value                = azurerm_storage_account.tfstate.name
+}
+
+output "container_name" {
+  value                = azurerm_storage_container.tfstate.name
+}
+
