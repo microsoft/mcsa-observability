bccc834e7c1d72aba4424db4b152107deb25af93
Srivatsa Hegde <srhegde@microsoft.com>
2023-07-17 17:44:14 -0700

    add remote tfstate initialization

--- Utils/scripts/Terraform/grafana-datasource/main.tf
+++ Utils/scripts/Terraform/grafana-datasource/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "<storage_account_name>"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
