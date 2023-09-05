b3cb7a9410575bf0c54bcdcdc5300b6f275eeb18
Srivatsa Hegde <srhegde@microsoft.com>
2023-09-05 13:39:00 -0700

    update conditional "and" syntax

--- AdxIngestFunctionApp/AdxIngestFunction.cs
+++ AdxIngestFunctionApp/AdxIngestFunction.cs
@@ -63,11 +63,11 @@ namespace Observability.AdxIngestFunctionApp
             {
                 batchUrl = $"https://{message.Location}.metrics.monitor.azure.com/subscriptions/{message.SubscriptionID}/metrics:getBatch?timespan={timeSpan}&interval=PT15M&metricnames=ServiceAvailability&aggregation=average&metricNamespace={message.Type}&autoadjusttimegrain=true&api-version=2023-03-01-preview";
             }
-            if (message.Type == "microsoft.cognitiveservices/accounts" and message.Metric == "Availability" )
+            if (message.Type == "microsoft.cognitiveservices/accounts" && message.Metric == "Availability" )
             {
                 batchUrl = $"https://{message.Location}.metrics.monitor.azure.com/subscriptions/{message.SubscriptionID}/metrics:getBatch?timespan={timeSpan}&interval=PT15M&metricnames=SuccessRate&aggregation=average&metricNamespace={message.Type}&autoadjusttimegrain=true&api-version=2023-03-01-preview";
             }
-            if (message.Type == "microsoft.cognitiveservices/accounts" and message.Metric == "GeneratedTokens" )
+            if (message.Type == "microsoft.cognitiveservices/accounts" && message.Metric == "GeneratedTokens" )
             {
                 batchUrl = $"https://{message.Location}.metrics.monitor.azure.com/subscriptions/{message.SubscriptionID}/metrics:getBatch?timespan={timeSpan}&interval=PT15M&metricnames={message.Metric}&filter=ApiName eq '*' and ModelDeploymentName eq '*'&aggregation=total&metricNamespace={message.Type}&autoadjusttimegrain=true&api-version=2023-03-01-preview";
             }
