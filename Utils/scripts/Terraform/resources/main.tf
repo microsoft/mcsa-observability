1250ddb9e69db2f165929d863f1c62cc17173697
Sruthi Kilari <sruthikilari@microsoft.com>
2024-01-10 09:44:58 -0800

    key vault changes

--- Utils/scripts/Terraform/resources/main.tf
+++ Utils/scripts/Terraform/resources/main.tf
@@ -279,6 +279,65 @@ resource "time_sleep" "wait_servicebus_creation" {
   create_duration = "10s"
 }
 
+#create key vault
+resource "azurerm_key_vault" "kv" {
+  name                        = "${var.prefix}-vault"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  depends_on = [azurerm_resource_group.rg]
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id #user
+
+    key_permissions = [
+      "Get",
+      "List"
+    ]
+
+    secret_permissions = [
+      "Get",
+      "Set",
+      "List"
+    ]
+
+    certificate_permissions = [
+      "Get",
+      "List"
+    ]
+  }
+}
+
+#add key vault access policy to msi
+resource "azurerm_key_vault_access_policy" "msiaccess" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_user_assigned_identity.terraform.principal_id #msi
+  depends_on = [azurerm_resource_group.rg, azurerm_key_vault.kv]
+
+  key_permissions = [
+      "Get",
+      "List"
+    ]
+
+    secret_permissions = [
+      "Get",
+      "Set",
+      "List"
+    ]
+
+    certificate_permissions = [
+      "Get",
+      "List"
+    ]
+}
+
 #create function apps
 resource "azurerm_service_plan" "timerstartpipelineapp" {
   name                = "timerstartpipelineapp-service-plan"
