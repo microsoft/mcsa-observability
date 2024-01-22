da916752280327ba4ff01dfcda17024edfaea876
Neerja Ginotra <neginotr@microsoft.com>
2024-01-22 09:30:50 -0800

    Added Tenant

---
+++ Utils/Data/Tenant.cs
@@ -0,0 +1,15 @@
+﻿namespace Observability.Utils.Data
+{
+    public class Tenant
+    {
+        public string ClientId { get; set; }
+        public string ClientSecretId { get; set; }
+        public string TenantId { get; set; }
+        
+
+        public Tenant()
+        {
+            ClientId = ClientSecretId = TenantId =  "";
+        }
+    }
+}
