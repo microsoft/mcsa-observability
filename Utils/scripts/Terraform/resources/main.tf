8235e4d3b8b51600defe245915da2e000ef782c5
onelhirmez <onelhirmez@microsoft.com>
2024-01-23 14:09:22 -0800

    Change terraform to mirror the shell script deployment

--- Utils/scripts/Terraform/resources/main.tf
+++ Utils/scripts/Terraform/resources/main.tf
@@ -331,6 +331,7 @@ resource "azurerm_windows_function_app" "timerstartpipelineapp" {
     msiclientId=azurerm_user_assigned_identity.terraform.client_id
     storagesas=data.azurerm_storage_account_sas.this.sas
     blobConnectionString=azurerm_storage_account.this.primary_connection_string
+    MyTimeTrigger="0 */1 * * * *"
 	}
 }
 
@@ -456,6 +457,7 @@ resource "azurerm_windows_function_app" "adxingestionapp" {
     msiclientId=azurerm_user_assigned_identity.terraform.client_id
     storagesas=data.azurerm_storage_account_sas.this.sas
     blobConnectionString=azurerm_storage_account.this.primary_connection_string
+    DefaultRequestHeaders="observabilitydashboard"
 	}
 
 }
